e1bd19f96722cadcc32954c385e0e954cb461939
CNaveen0101 <143951163+CNaveen0101@users.noreply.github.com>
2026-08-13 19:44:34 +0530

    Create variable.tf

---
+++ Dev/EC-Instance/variable.tf
@@ -0,0 +1,19 @@
+variable "ami" {
+    description = "value of the ami"
+  
+}
+
+variable "instance_type" {
+    description = "value of the instance type"
+  
+}
+
+variable "subnet_id" {
+    description = "value of the subnet_id"
+  
+}
+
+variable "vpc_security_group_ids" {
+    description = "value of the security group"
+  
+}
